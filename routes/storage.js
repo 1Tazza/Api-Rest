3d209b51ecea9f2e622d4bbf473f8946582dd7c8
unknown <tazza.personal@gmail.com>
2023-07-27 12:55:23 -0300

    storage to controller

--- routes/storage.js
+++ routes/storage.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { __dirname } from "../utils/utils.js";
 import uploadMiddleware from "../utils/handleStorage.js";
+import { createItem } from "../controllers/storage.js";
+
 const router = express()
 
 
-router.post("/", uploadMiddleware.single("myfile") ,(req, res) => {
-    res.send({a: 1})
-})
+router.post("/", uploadMiddleware.single("myfile") ,createItem)
 
 export default router
