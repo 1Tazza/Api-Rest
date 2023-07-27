3a36f247d8c222f5ba633579e2a892c50f9f8027
unknown <tazza.personal@gmail.com>
2023-07-27 12:42:13 -0300

    storage img

--- routes/storage.js
+++ routes/storage.js
@@ -1,8 +1,31 @@
 import express from "express";
+import multer from "multer";
+import { __dirname } from "../utils/utils.js";
 const router = express()
 
-router.post("/", (req, res) => {
-    
+
+/**
+ *  
+ */
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        const pathStorage = `${__dirname}/../storage`;
+        cb(null, pathStorage) 
+    },
+    filename: function(req, file, cb) {
+        const ext = file.originalname.split(".").pop();
+        const filename = `file-${Date.now()}.${ext}`;
+        cb(null, filename)
+    }
+});
+
+const uploadMiddleware = multer({storage})
+/**
+*
+*/
+
+router.post("/", uploadMiddleware.single("myfile") ,(req, res) => {
+    res.send({a: 1})
 })
 
 export default router
