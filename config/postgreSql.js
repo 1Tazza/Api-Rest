f89c9aeedc75a681e7fc9e082723b1d709fbb015
unknown <tazza.personal@gmail.com>
2023-08-03 21:19:19 -0300

    postgreSql function added

--- config/postgreSql.js
+++ config/postgreSql.js
@@ -1,1 +1,21 @@
-import {sequelize} from "sequelize"
+import {Sequelize} from "sequelize"
+
+const sequelize = new Sequelize(
+    database,
+    username,
+    password,
+    {
+      host,
+      dialect: "postgresql"
+    }
+)
+
+
+const dbConnectPostgre = async () => {
+    try{
+
+    }catch(e){
+        console.log("POSTGRESQL ERROR")
+    }
+
+}
