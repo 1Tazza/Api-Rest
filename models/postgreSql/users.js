b06c83f2768fcbd4000075e2f7352828f59153d2
unknown <tazza.personal@gmail.com>
2023-08-06 13:24:57 -0300

    users model postgresql added

---
+++ models/postgreSql/users.js
@@ -0,0 +1,28 @@
+import sequelize from "../../config/postgreSql.js"
+import { DataTypes } from "sequelize"
+
+const User = sequelize.define(
+    "users",
+    {
+     name: {
+        type: DataTypes.STRING,
+        allowNull: false
+     },
+     age: {
+        type: DataTypes.NUMBER
+     },
+     email: {
+        type: DataTypes.STRING
+     },
+     password: {
+        type: DataTypes.STRING
+     },
+     role: {
+        type: DataTypes.ENUM(["user", "admin"])
+     }
+
+    },
+    {
+        timestamps: true
+    }
+)
